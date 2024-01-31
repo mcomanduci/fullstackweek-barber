1bf792d1ba38a888368e682611c9373bc34797fa
Marcelo Comanduci <mcomanduci@gmail.com>
2024-01-31 04:31:09 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,8 +8,8 @@ import BarbershopItem from "./_components/barbershop-item";
 
 export default async function Home() {
   // chamar prisma e pegar barbearias
-  const barbershops = await db.barbershop.findMany({})
-  
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
@@ -23,24 +23,41 @@ export default async function Home() {
         </p>
       </div>
 
-      <div className="px-5 mt-6">
+      <div className="mt-6 px-5">
         <Search />
       </div>
 
-      <div className="px-5 mt-6">
-        <h2 className='text-xs uppercase text-gray-400 font-bold mb-3'>Agendamentos</h2>
-          <BookingItem />
+      <div className="mt-6 px-5">
+        <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <BookingItem />
       </div>
 
-      <div className='mt-6'>
-        <h2 className='px-5 text-xs uppercase text-gray-400 font-bold mb-3'>Recomendados</h2>
+      <div className="mt-6">
+        <h2 className="mb-3 px-5 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
 
-        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop:any) => (
+        <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: any) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="mb-3 px-5 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+
+        <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: any) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 }
